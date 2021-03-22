7b524872aae8888bd80ee1b18f7b936020d4b6e2
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-22 07:01:39 -0300

    day zero completed

--- lib/thirty_days_coding.ex
+++ lib/thirty_days_coding.ex
@@ -1,18 +1,2 @@
 defmodule ThirtyDaysCoding do
-  @moduledoc """
-  Documentation for `ThirtyDaysCoding`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ThirtyDaysCoding.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
---
+++ lib/zero_hello_world.ex
@@ -0,0 +1,6 @@
+defmodule ThirtyDaysCoding.ZeroHelloWorld do
+  IO.puts("Hello, World.")
+
+  input_string = IO.gets("")
+  IO.puts(input_string)
+end

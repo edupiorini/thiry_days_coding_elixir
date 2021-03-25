200558a4392351bcbd91105b93249dc2e6eaa732
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-25 12:56:54 -0300

    first structure set

---
+++ lib/dayfour/src/App.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class App {
+    private int age;
+
+    public Person(int initialAge) {
+  		// Add some more code to run some checks on initialAge
+	}
+
+    public void amIOld() {
+        // Write code determining if this person's age is old and print the correct
+        // statement:
+        System.out.println(/* Insert correct print statement here */);
+    }
+
+    public void yearPasses() {
+  		// Increment this person's age.
+	}
+
+public static void main(String[] args) {

fc0c7d5d7c98b8deb1780e2affe07bc219223ae2
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-25 13:00:07 -0300

    constructor and verifications set

--- lib/dayfour/src/App.java
+++ lib/dayfour/src/App.java
@@ -4,9 +4,13 @@ import java.util.*;
 public class App {
     private int age;
 
-    public Person(int initialAge) {
-  		// Add some more code to run some checks on initialAge
-	}
+    public App(int initialAge) {
+        if (initialAge < 0) {
+            System.out.println("Age is not valid, setting age to 0.");
+            this.age = 0;
+        } else
+            this.age = initialAge;
+    }
 
     public void amIOld() {
         // Write code determining if this person's age is old and print the correct
